dd18d7defb83190bad7e4aa3dab8cc875447134d
Franklin-93 <102427836+Franklin-93@users.noreply.github.com>
2023-04-26 13:43:02 +0100

    Adding Functionalities

--- src/app/renderer.js
+++ src/app/renderer.js
@@ -1,34 +1,54 @@
 
-
-
-const btn = document.querySelector(".calculate");
+const box_1 = document.querySelector(".box_1");
+const button = document.querySelector(".button");
 const userItem = document.getElementById("item");
 let itemValue = document.getElementById("price");
 let length = document.getElementById("length");
+let result_part_1 = document.querySelector(".results-part-1");
 
 
-function Go(){
 
-    let price = parseInt(itemValue.value);
-    let instalments = parseInt(length.value);
-    var result = price / instalments;
+ // event listeneing on click will display monthly value
+ button.addEventListener("click", getMonthlyValue);
+ 
+// getMonthlyValue
+
+function getMonthlyValue() {   
+     //preventDefault(); // prevents the page to reload      
+  
+    let price = parseInt(itemValue.value); // get prince
+    let instalments = parseInt(length.value); // get number of months
+    var result = price / instalments; // find the monthly value
+
+    // appends the monthly value to the DOM
+    result_part_1.innerHTML = " The " + userItem.value +  " is going tHHHHHHHHHHHHo cost you " +  Math.round(result) + " Monthly ";
+   
     
-    // append to the 
-    document.querySelector(".results-part-1").innerHTML = " The " + userItem.value +  " is going to cost you " +  Math.round(result) + " Monthly "    
-    
+  };
+
+   // event listeneing on click will display monthly value
+ //button.addEventListener("click", getMonthlyValue);
 
 
-btn.addEventListener('click', () => {
-  // 👇️ hide button
-  btn.style.display = 'none';
 
-  // 👇️ show div
-  const box = document.querySelector('.results-part-1');
-  box.style.display = 'block';
-});
+/*function hideForm(){
+    button.addEventListener('click', () => {
+    //button.style.display = 'none';
 
-event.preventDefault(); 
+    // hide form
+    box_1.style.display = 'none';
+
+    // show div results for the item
+    const result = document.querySelector('.results-part-1');
+    result.style.display = 'block';
+
+    // shows second form 
+  });
+
 };
+
+hideForm();*/
+
 
 
 
