5faf57735658ee9bb13801c24de07cea82e2a8e5
Franklin-93 <102427836+Franklin-93@users.noreply.github.com>
2023-04-27 15:48:57 +0100

    Onclick and Create Input Function Finished

--- src/app/renderer.js
+++ src/app/renderer.js
@@ -1,54 +1,92 @@
 
+// Getting Values
 const box_1 = document.querySelector(".box_1");
 const button = document.querySelector(".button");
 const userItem = document.getElementById("item");
+const result_part_1 = document.querySelector(".results-part-1");
+const results = document.querySelector(".results");
+const myForm = document.querySelector(".form-inline-1");
+
+// Getting Price / Length
 let itemValue = document.getElementById("price");
 let length = document.getElementById("length");
-let result_part_1 = document.querySelector(".results-part-1");
 
 
 
- // event listeneing on click will display monthly value
- button.addEventListener("click", getMonthlyValue);
- 
-// getMonthlyValue
 
-function getMonthlyValue() {   
-     //preventDefault(); // prevents the page to reload      
-  
+
+
+
+
+// onclick function when button 'next' is clicked
+  button.addEventListener("click", function (event) {
+
     let price = parseInt(itemValue.value); // get prince
     let instalments = parseInt(length.value); // get number of months
-    var result = price / instalments; // find the monthly value
+    let result = price / instalments; // find the monthly value
 
+    let text = " The " + userItem.value +  " is going to cost you " +  Math.round(result) + " Monthly ";
     // appends the monthly value to the DOM
-    result_part_1.innerHTML = " The " + userItem.value +  " is going tHHHHHHHHHHHHo cost you " +  Math.round(result) + " Monthly ";
-   
-    
-  };
+    result_part_1.innerHTML = text;  
 
-   // event listeneing on click will display monthly value
- //button.addEventListener("click", getMonthlyValue);
-
-
-
-/*function hideForm(){
-    button.addEventListener('click', () => {
-    //button.style.display = 'none';
-
-    // hide form
+    //-------------------------------------------
+    // hide form when button next is clicked
     box_1.style.display = 'none';
-
+      
     // show div results for the item
-    const result = document.querySelector('.results-part-1');
-    result.style.display = 'block';
+    results.style.display = 'block';
+    //-------------------------------------------
+  
+    // create form after 2 seconds using 'setTimeOut' method
+    setTimeout(createInput, 2000);
 
-    // shows second form 
-  });
+    // define the 'event' in the function parameter and call prevent default to avoid loading the page
+    event.preventDefault();
 
-};
+    // assigning the 'myForm' class to reset method so the form can be cleared once the button is clicked
+    myForm.reset();
 
-hideForm();*/
+ });
+  
 
+
+
+
+
+
+
+ 
+
+
+  // Create ELEMENTS...
+  function createInput(){
+
+    //Create a 'div' Container element
+    let divElement = document.createElement('div'); // create 'div' element
+    divElement.textContent = "container";  // text content
+    divElement.setAttribute('id', 'myDiv'); // setting 'ID'
+    // or >   divElement.id = "myDiv";
+
+    // Append 'div' to the DOM
+    document.body.appendChild(divElement);
+
+    // Create an 'input' element
+    let input = document.createElement('input'); 
+
+    // Getting the div's ID and appending to input created
+    document.getElementById('myDiv').append(input);
+
+    // create a calculate button
+    let button = document.createElement("button");
+    button.textContent = "calculate";  // text content
+    button.setAttribute('id', 'btn'); // setting 'ID'
+
+    // append button to the 'div' element
+    divElement.appendChild(button);
+  }
+  
+
+  
 
 
 
