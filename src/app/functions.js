30fe2e3edfdc5134641ccfdd50c93f465f0ec64f
Franklin-93 <102427836+Franklin-93@users.noreply.github.com>
2023-05-04 14:54:43 +0100

    Export & Import Functions

--- src/app/functions.js
+++ src/app/functions.js
@@ -1,132 +1,149 @@
-
-
-    
-    // GLOBAL static variable that holds the days of the month
-    const DAYS_OF_THE_MONTH = 30;
+ // GLOBAL static variable that holds the days of the month
+ const DAYS_OF_THE_MONTH = 30;
    
-    /**
-     * Finding MONTHLY value
-     * Item value = 3600 (total)
-     * numberOfInstalments = 4 (months)
-     * Monthly value > 3600/4 = 900
-     * ===================================================
-     * @param itemValue original amount entered by the user
-     * @param length months the user chooses to pay
-     * @param userItem display item / object name
-     * @return monthly value 
-     */
-    
-    function getMonthlyValue(itemValue, length, userItem) {
-        let monthlyValue = itemValue/length;
-        System.out.println("The " + userItem + " is going to cost you " + Math.round(monthlyValue) + " monthly \n");
+ /**
+  * Finding MONTHLY value
+  * Item value = 3600 (total)
+  * numberOfInstalments = 4 (months)
+  * Monthly value > 3600/4 = 900
+  * ===================================================
+  * @param itemValue original amount entered by the user
+  * @param length months the user chooses to pay
+  * @return monthly value 
+  */
+ 
+ function getMonthlyValue(itemValue, length) {
+
+    let value = parseInt(itemValue); 
+    let instalments = parseInt(length);
+    let monthlyValue = value/instalments;
         return Math.round(monthlyValue); 
-    }
+ };
 
-    /**
-     * Rule of Three (DIRECT) to find total months either for less or more
-     * ==============================================================================================
-     * @param length number of instalments that the user wants to pay (number of months)
-     * @param itemValue original amount chosen by the user  
-     * @param userInstalments is how much the user could pay  
-     * @return remaining months (difference of months more to pay)
-     */
-    function getMonthsDueToPay(length, itemValue, userInstalments) {
-        
-           let userStatus = (userInstalments * length);          // 4 x 85 = 340 euros 
-           let days = length * DAYS_OF_THE_MONTH;                // 4 x 30 = 120 days 
-           let totalDays = (itemValue * days / userStatus);      // 280 * 120 = 33600
-           let monthsToPay = totalDays / DAYS_OF_THE_MONTH;      // 33600 / 340 = 98.82 days
-                                                                    // 98.82 / 30 days = 3.29 months                                                       
-      /**  MONTHS DUE                 |  DECIMAL PART
-       *  ============                |  ==============
-       * 340.x = 280*120              |  100.X = 30*29
-       *  340.x = 33600               |   100.X = 870 
-       *    x = 33600                 |    X = 870
-       *        /340                  |      /100
-       *     x = 98.82                |       X = 7.8 DAYS (MUST BE ROUNDED TO UP)
-       *        /30 = 3.29 Months     |
-       */
-        return monthsToPay; 
-    }
-    
-    
-    
-    /**
-     * GET DECIMAL from the remaining months to count the days left
-     * =============================================================
-     * @param remainingMonths from the getRemainingMonths (method) in the parameter 
-     * @return the extracted DECIMAL value from the remaining months
-     */
-    function getDecimal(remainingMonths) {
-        let value = remainingMonths.toString().indexOf(".");
-        let decimal = number.toString().substring(value+1);
-        return decimal; // DONT ROUND THE DECIMAL AS it is going to return a integer (eg. 0.5 decimal = 1.0 rounded)
-    }
+ /**
+  * Rule of Three (DIRECT) to find total months either for less or more
+  * ==============================================================================================
+  * @param length number of instalments that the user wants to pay (number of months)
+  * @param itemValue original amount chosen by the user  
+  * @param userInstalments is how much the user could pay  
+  * @return remaining months (difference of months more to pay)
+  */
+ function getMonthsDueToPay(itemValue, length, userInstalments) {
+     
+        let userStatus = (userInstalments * length);          // 4 x 85 = 340 euros 
+        let days = length * DAYS_OF_THE_MONTH;                // 4 x 30 = 120 days 
+        let totalDays = (itemValue * days / userStatus);      // 280 * 120 = 33600
+        let monthsToPay = totalDays / DAYS_OF_THE_MONTH;      // 33600 / 340 = 98.82 days
+                                                                 // 98.82 / 30 days = 3.29 months                                                       
+   /**  MONTHS DUE                 |  DECIMAL PART
+    *  ============                |  ==============
+    * 340.x = 280*120              |  100.X = 30*29
+    *  340.x = 33600               |   100.X = 870 
+    *    x = 33600                 |    X = 870
+    *        /340                  |      /100
+    *     x = 98.82                |       X = 7.8 DAYS (MUST BE ROUNDED TO UP)
+    *        /30 = 3.29 Months     |
+    */
+     return monthsToPay; 
+ };
+ 
+ 
+ 
+ /**
+  * GET DECIMAL from the remaining months to count the days left
+  * =============================================================
+  * @param remainingMonths from the getRemainingMonths (method) in the parameter 
+  * @return the extracted DECIMAL value from the remaining months
+  */
 
-    /**
-     * GET INTEGER from the remaining months 
-     * ============================================================= 
-     * @param differenceMonths get the difference in months either (less or more)
-     * @return the extracted INTEGER value from the remaining months
-     */
-    function getInteger(differenceMonths) {
-        let integer = Math.trunc(differenceMonths);
-        return integer; 
-    }
-    
-    
-    /**
-     * Rule of Three (INVERSE) to find remaning days from decimal part
-     * ================================================================
-     * Will use the decimal part multiplied by total days of the month = (30 * 0.6 = 19.5 days total)
-     * @param decimal taken from the getDecimal (method) in the parameter
-     * @return the remaining days from the decimal part
-     */
-    function getRemaningDays(decimal) {
-        let daysFromDecimalPart = (int) (DAYS_OF_THE_MONTH * decimal);
-        return daysFromDecimalPart;
-    }
-   
-    
-    /**
-     * Works the same as the (getRemainingDays) the only difference
-     * is that this one is going to return rounded type
-     * ==============================================================
-     * @param decimal
-     * @return ROUNDED 
-     */
-    function getRemaningDaysRounded(decimal) {
-        let daysFromInteger = DAYS_OF_THE_MONTH * decimal;
-        return Math.ceil(daysFromInteger);
-    }
-    
+ 
+// REFERENCE IN  JavaScript 
+// https://www.geeksforgeeks.org/how-to-get-decimal-portion-of-a-number-using-javascript/
+// OR
+//https://www.javascripttutorial.net/javascript-remainder-operator/
 
-    /**
-     * @param length
-     * @param integer
-     * @return THE difference between length & integer for LESS
-     */
-    function getDifferenceLess(length, integer) {
-        let totalMonths = length - integer;
-        return totalMonths; 
-    }
-    
-    
-    /**
-     * @param length
-     * @param integer
-     * @return THE difference between length & integer for MORE
-     */
-    function getDifferenceMore(length, integer) {
-        let totalMonths = integer - length ;
-        return totalMonths;
-    }
+ function getDecimal(remainingMonths) {
+     let remainder = remainingMonths % 1;
+     //let Decimalvalue = remainingMonths.toString().split(".")[1];
+     return remainder; // DONT ROUND THE DECIMAL AS it is going to return a integer (eg. 0.5 decimal = 1.0 rounded)
+ };
+
+ /**
+  * GET INTEGER from the remaining months 
+  * ============================================================= 
+  * @param differenceMonths get the difference in months either (less or more)
+  * @return the extracted INTEGER value from the remaining months
+  * The Math.trunc() static method returns the integer part of a number by removing any fractional digits
+  * 
+  */
+ function getInteger(differenceMonths) {
+     let integer = Math.trunc(differenceMonths);
+     return integer; 
+ };
+ 
+ 
+ /**
+  * Rule of Three (INVERSE) to find remaning days from decimal part
+  * ================================================================
+  * Will use the decimal part multiplied by total days of the month = (30 * 0.6 = 19.5 days total)
+  * @param decimal taken from the getDecimal (method) in the parameter
+  * @return the remaining days from the decimal part
+  * The Math.ceil() static method always rounds up and returns the smaller integer greater than or equal to a given number
+  */
+ function getRemaningDays(decimal) {
+     let daysFromDecimalPart = DAYS_OF_THE_MONTH * decimal;
+     return  Math.ceil(daysFromDecimalPart);
+     
+ };
+
+ 
+ /**
+  * Works the same as the (getRemainingDays) the only difference
+  * is that this one is going to return rounded type
+  * ==============================================================
+  * @param decimal
+  * @return ROUNDED 
+  */
+ function getRemaningDaysRounded(decimal) {
+     let daysFromInteger = DAYS_OF_THE_MONTH * decimal;
+     return Math.ceil(daysFromInteger);
+ };
+ 
+
+ /**
+  * @param length
+  * @param integer
+  * @return THE difference between length & integer for LESS
+  */
+ function getDifferenceLess(length, integer) {
+     let totalMonths = length - integer;
+     return totalMonths; 
+ };
+ 
+ 
+ /**
+  * @param length
+  * @param integer
+  * @return THE difference between length & integer for MORE
+  */
+ function getDifferenceMore(length, integer) {
+     let totalMonths = integer - length ;
+     return totalMonths;
+ };
 
    // exportiing the functions to the main where we want to use it
-    module.exports = {
+   export { getMonthlyValue, getMonthsDueToPay, getDecimal,
+
+    getInteger, getRemaningDays, getRemaningDaysRounded,
+
+    getDifferenceLess, getDifferenceMore,
+};
+  
+
+/*module.exports = {
         getMonthlyValue, getMonthsDueToPay, getDecimal,
 
         getInteger, getRemaningDays, getRemaningDaysRounded,
     
-        getDifferenceLess, getDifferenceMore
-    }
+        getDifferenceLess, getDifferenceMore,
+    }*/
--- src/app/renderer.js
+++ src/app/renderer.js
@@ -1,9 +1,59 @@
 
 // we use require when usiing node.js
 // assign them to const and then go about just calling them
-const myFunctions = require("./functions");
+import * as myCalculator from './functions.js'
 
-// Getting Values
+
+/*-------------------------------------------------------------
+Getting DECIMAL PART from months due to pay
+--------------------------------------------------------------*/
+//let decimalPart = myCalculator.getDecimal(monthsDue);
+//console.log("EXTRACTING DECIMAL PART : " + decimalPart );
+
+/*-------------------------------------------------------------
+Getting INTEGER PART from months due to pay
+--------------------------------------------------------------*/
+//let integer = myCalculator.getInteger(monthsDue);
+//console.log("INTEGER PART : " + integer);
+
+/*-------------------------------------------------------------
+Getting DAYS from decimal part of months due (rounded to UP)
+--------------------------------------------------------------*/
+//let daysFromDecimal = myCalculator.getRemaningDaysRounded(decimalPart);
+//console.log("DAYS FROM DECIMAL PART : (make sure to round to 20) " + daysFromDecimal);
+
+ /*-------------------------------------------------------------
+(IF user is PAYING MORE) that will leave them with LESS months to pay
+Finding DIFFERENCE between (length & MonthsDue) 
+--------------------------------------------------------------*/
+//let difference = myCalculator.getDifferenceMore(29,monthsDue);       
+//console.log("DIFFERENCE BETWEEN (length & months Due - MORE : " + difference);
+
+/*-------------------------------------------------------------
+(IF user is PAYING LESS) that will leave them with MORE months to pay
+Finding DIFFERENCE between (length & MonthsDue) 
+--------------------------------------------------------------*/
+//let difference_2 = myCalculator.getDifferenceMore(29,monthsDue);       
+//console.log("DIFFERENCE BETWEEN (length & months Due - MORE : " + difference_2);
+
+/*-------------------------------------------------------------
+Extracting DECIMAL PART from Difference
+--------------------------------------------------------------*/
+//let getDecimalFromDifference = myCalculator.getDecimal(difference);
+//console.log("EXTRACTING DECIMAL PART from DIFFERENCE - LESS : " + getDecimalFromDifference );
+
+//let getDecimalFromDifference_2 = myCalculator.getDecimal(difference_2);
+//console.log("EXTRACTING DECIMAL PART from DIFFERENCE - MORE : " + getDecimalFromDifference_2 );
+
+/*-------------------------------------------------------------
+Getting days from (Integer Difference) to use if USER IS PAYING More
+--------------------------------------------------------------*/
+//let daysFromDifferenceMonths = myCalculator.getRemaningDays(getDecimalFromDifference);
+//console.log("DAYS FROM DECIMAL PART ( difference ) - LESS : " + daysFromDifferenceMonths);
+
+
+  
+// Getting Values from DOM
 const box_1 = document.querySelector(".box_1");
 const button = document.querySelector(".button");
 const userItem = document.getElementById("item");
@@ -11,46 +61,140 @@ const result_part_1 = document.querySelector(".results-part-1");
 const results = document.querySelector(".output-container");
 const myForm = document.querySelector(".form-inline-1");
 const PDFStart = document.querySelector(".pdf-start");
-
-// Getting Price / Length
 let itemValue = document.getElementById("price");
 let length = document.getElementById("length");
+let div = document.getElementById("myDiv");
+let result_part_2 = document.querySelector(".results-part-2");
+let button_2 = document.getElementById("button_2");
+let userInput = document.getElementById("input");
 
 
-// onclick function when button 'next' is clicked
-  button.addEventListener("click", function (event) {
-    let price = parseInt(itemValue.value); // get prince
-    let instalments = parseInt(length.value); // get number of months
-    let result = price / instalments; // find the monthly value
 
-    let text = " The " + userItem.value +  " is going to cost you " +  Math.round(result) + " Monthly ";
-    // appends the monthly value to the DOM
-    result_part_1.innerHTML = text;  
+// onclick call get first breakdown input when button 'next' is clicked
+button.addEventListener("click", function(event) {
+      
+       // 1) CALL getMonthlyValue - Finding (months due to pay) 
+       let monthlyValue = myCalculator.getMonthlyValue(itemValue.value,length.value); 
+        // monthly output
+        let text = " The " + userItem.value +  " is going to cost you " + monthlyValue + " Monthly ";
 
-    //-------------------------------------------
+        // appends the monthly value to the DOM
+        result_part_1.innerHTML = text;  
 
-    // hide form when button next is clicked
-    box_1.style.display = 'none';
-   
-    // show div results for the item
-    results.style.display = 'block';
+        // hide form when button next is clicked
+        box_1.style.display = 'none';
+
+        // show div results for the item
+        results.style.display = 'block';
+        
+        // shows DIV
+        showDIV();
+
+        // define the 'event' in the function parameter and call prevent default to avoid loading the page
+        event.preventDefault();
+
+        // assigning the 'myForm' class to reset method so the form can be cleared once the button is clicked
+       // myForm.reset();
+    });
+
+
+
+
+  // shows DIV
+  function showDIV(){
+      setTimeout(() => {
+      // it was set to display 'none' in CSS
+      //here when button is clicked call function inside first event listener
+      div.style.display = 'block';
+    }, 4000); 
+  };
     
+
+    
+
+
+    
+  // onclick function when button 'next' is clicked
+  button_2.addEventListener("click", function (event) {
+
+     /*-------------------------------------------------------------
+    Finding (months due to pay) 
+    --------------------------------------------------------------*/
+    let monthsDue = myCalculator.getMonthsDueToPay(itemValue.value,length.value,userInput.value);
+
+    let text2 = "just (TESTING) MONTHS TO PAY " + monthsDue;
+
+    // appends the monthly value to the DOM
+    result_part_2.innerHTML = text2;  
+
     //-------------------------------------------
-  
-    // create form after 2 seconds using 'setTimeOut' method
-    setTimeout(createInput,2000);
+    // hide form when button next is clicked
+    div.style.display = 'none';
+      
+    // Hide first results
+    result_part_1.style.display ="none";
+
+    // show div results for the item
+    result_part_2.style.display = 'block';
+
+    // shows the PDF generator and start page again
+    PDFStart.style.display = 'block';
+    //-------------------------------------------
 
     // define the 'event' in the function parameter and call prevent default to avoid loading the page
     event.preventDefault();
+})
 
-    // assigning the 'myForm' class to reset method so the form can be cleared once the button is clicked
-    myForm.reset();
 
- });
-  
 
-  // Create Div Elements and Input form along with the button dinamicly when button next is clekced
-  //so the last question(how can the user pay for the 'x' item ?)...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+  // Create Div Elements and Input form along with the button dinamicly when
+  // button next is clekced so the last question(how can the user pay for the 'x' item ?)...
   function createInput(){
 
     //Create a 'div' Container element
@@ -65,7 +209,9 @@ let length = document.getElementById("length");
     // Create an 'input' element
     let input = document.createElement('input'); 
     input.id="input";
+    //let userInstalments = document.getElementById('input');
     
+        
     // Getting the div's ID and appending to input created
     document.getElementById('myDiv').append(input);
 
@@ -86,59 +232,7 @@ let length = document.getElementById("length");
     // calling Function after creating a div form
     myTEST();
   }
-  
-  
-
-function myTEST(){
-  let div = document.getElementById("myDiv");
-  let result_part_2 = document.querySelector(".results-part-2");
-  let button_2 = document.getElementById("button_2");
-  let userInput = document.getElementById("input");
-
-  // onclick function when button 'next' is clicked
-  button_2.addEventListener("click", function (event) {
-  let text2 = " Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae quas rem necessitatibus perspiciatis deserunt modi hic voluptatibus commodi facilis earum! Quos, repellendus laudantium? Atque asperiores temporibus suscipit sint aperiam exercitationem"+
-   + "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Recusandae quas rem necessitatibus perspiciatis deserunt modi hic voluptatibus commodi facilis earum! Quos, repellendus laudantium? Atque asperiores temporibus suscipit sint aperiam exercitationem." + userInput.value;
-
-  // appends the monthly value to the DOM
-  result_part_2.innerHTML = text2;  
-
-  //-------------------------------------------
-  // hide form when button next is clicked
-  div.style.display = 'none';
-    
-  // Hide first results
-  result_part_1.style.display ="none";
-
-  // show div results for the item
-  result_part_2.style.display = 'block';
-
-  // shows the PDF generator and start page again
-  PDFStart.style.display = 'block';
-  //-------------------------------------------
-
-  // define the 'event' in the function parameter and call prevent default to avoid loading the page
-  event.preventDefault();
-})
-};
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
+  */
 
   
 
